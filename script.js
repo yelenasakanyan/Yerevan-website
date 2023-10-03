d4135ae4409152aa0e00b50b0424fb5c45ef759b
Your Name <you@example.com>
2023-10-03 15:18:25 +0300

    the initial version of website

---
+++ script.js
@@ -0,0 +1,5 @@
+const img = document.createElement('img');
+img.src = 'Skyball-FEATURED-IMAGE-1500x550px.jpg';
+document.getElementById("new_line").append(img);
+img.style = "height: 800px";
+console.log(img);
